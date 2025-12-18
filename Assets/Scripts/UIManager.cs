f6912b12b82bd7bb9ce768602b377bc9c6d66600
Rafael Marasco <55808458+rafaelmarasco@users.noreply.github.com>
2025-12-17 23:11:15 -0400

    Rework

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class UIManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else 
+        Application.Quit();
+#endif
+    }
+
+}
